ab81849fa1bee9f2fc598dd287bc4bf3c14afad9
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-09-06 22:37:23 +0100

    [P13] Run-length encoding of a list (direct solution)

--- src/main/scala/lists/ListHelper.scala
+++ src/main/scala/lists/ListHelper.scala
@@ -78,4 +78,14 @@ object ListHelper {
 
   def decode[A](input: List[(Int, A)]): List[A] = input.flatMap(elem => List.fill(elem._1)(elem._2))
 
+  def encodeDirect[A](input: List[A]): List[(Int, A)] =
+    input.foldLeft(List.empty[(Int, A)]) { (list, elem) =>
+      list match {
+        case Nil => List((1, elem))
+        case initialTuples :+ lastTuple if lastTuple._2 == elem => initialTuples :+ (lastTuple._1 + 1, elem)
+        case _ => list :+ (1, elem)
+      }
+    }
+
+
 }
--- src/test/scala/lists/ListHelperTest.scala
+++ src/test/scala/lists/ListHelperTest.scala
@@ -435,4 +435,39 @@ class ListHelperTest extends WordSpec {
     }
   }
 
+  "The function encodeDirect" should {
+    "return correct list" when {
+
+      "provided with empty list" in {
+        val list = List.empty[Symbol]
+        assert(encode(list) == List.empty)
+      }
+
+      "provided with single-element list" in {
+        val list = List('a)
+        assert(encodeDirect(list) == List((1, 'a)))
+      }
+
+      "provided with list containing 3 different elements" in {
+        val list = List('a, 'b, 'c)
+        assert(encodeDirect(list) == List((1, 'a), (1, 'b), (1, 'c)))
+      }
+
+      "provided with list containing same elements but not next to each other" in {
+        val list = List('a, 'b, 'c, 'a, 'b)
+        assert(encodeDirect(list) == List((1, 'a), (1, 'b), (1, 'c), (1, 'a), (1, 'b)))
+      }
+
+      "provided with list containing the same 3 elements" in {
+        val list = List('a, 'a, 'a)
+        assert(encodeDirect(list) == List((3, 'a)))
+      }
+
+      "provided with list containing different elements but appearing in groups" in {
+        val list = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+        assert(encodeDirect(list) == List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+      }
+    }
+  }
+
 }
